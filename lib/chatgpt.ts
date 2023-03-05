3e7e126a4f3e1f0e039eb4fb2850aa906d08ccb1
Dylan <34380246+DKotzer@users.noreply.github.com>
2023-03-05 11:01:33 -0500

    removed org

--- lib/chatgpt.ts
+++ lib/chatgpt.ts
@@ -2,7 +2,6 @@ import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.NEXT_PUBLIC_KEY,
-  organization: "org-qaFpK5RoJjLWjlPBEJSM2yAP",
 });
 
 const openai = new OpenAIApi(configuration);
