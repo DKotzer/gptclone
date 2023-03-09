11c9b97df62f598d792107b99ccc72c9d8e40c3c
Dylan <34380246+DKotzer@users.noreply.github.com>
2023-03-09 13:35:04 -0500

    mobility fixes pt3

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -38,7 +38,7 @@ function ChatRow({ id }: Props) {
       href={`/chat/${id}`}
     >
       <ChatBubbleLeftIcon className='h-5 w-5 justify-start' />
-      <p className='flex-1 hidden md:inline-flex truncate max-w-[120px] overflow-ellipsis '>
+      <p className='flex-1 md:inline-flex truncate max-w-[120px] overflow-ellipsis '>
         {messages[0]?.messages.length < 3
           ? "New Chat"
           : messages[0]?.messages[messages[0]?.messages.length - 2].content}
