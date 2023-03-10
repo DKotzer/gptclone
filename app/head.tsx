9e3a7d77ae41498ae779617159377ce68e5aaa5c
Dylan <34380246+DKotzer@users.noreply.github.com>
2023-03-10 17:02:20 -0500

    another test

--- app/head.tsx
+++ app/head.tsx
@@ -7,7 +7,9 @@ export default function Head() {
       <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <meta name='description' content='An AI-powered Chatbot' />
-      <link rel='icon' href='%PUBLIC_URL%/dgpt.png' />
+      <link rel='icon' href='/dgpt.png' />
+      <meta property='og:image' content='/DylanGPTLogo.png' />
+
       <link
         rel='stylesheet'
         href='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/atom-one-dark-reasonable.min.css'
