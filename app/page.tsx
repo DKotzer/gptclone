29ab5dc1ff9f49b90e10e52cc3130a88cb8cc2ba
Dylan <34380246+DKotzer@users.noreply.github.com>
2023-03-10 00:03:53 -0500

    move over 4%

--- app/page.tsx
+++ app/page.tsx
@@ -13,59 +13,64 @@ function HomePage() {
     setWinHeight(`h-[${window.innerHeight}px]`);
   }, []);
   return (
-    <div
-      className={`flex flex-col items-center justify-center h-full text-white px-2`}
-    >
-      <h1 className='text-5xl font-bold mb-20'>DylanGPT</h1>
-      <div className='flex space-x-2 text-center'>
-        <div>
-          <div className='flex flex-col items-center justify-center mb-5'>
-            <SunIcon className='h-8 w-8' />
-            <h2>Examples</h2>
-          </div>
+    <div className={`flex flex-col h-screen overflow-hidden`}>
+      <div
+        className={`flex flex-col items-center justify-center h-full text-white px-2`}
+      >
+        <h1 className='text-5xl font-bold mb-20'>DylanGPT</h1>
+        <div className='flex space-x-2 text-center'>
+          <div>
+            <div className='flex flex-col items-center justify-center mb-5'>
+              <SunIcon className='h-8 w-8' />
+              <h2>Examples</h2>
+            </div>
 
-          <div className='space-y-2'>
-            <p className='infoText'>List the top things to do in Toronto</p>
-            <p className='infoText'>Why should I hire Dylan Kotzer.</p>
-            <p className='infoText'>
-              Create code for hello world in Python, Javascript and C++.
-            </p>
+            <div className='space-y-2'>
+              <p className='infoText'>List the top things to do in Toronto</p>
+              <p className='infoText'>Why should I hire Dylan Kotzer.</p>
+              <p className='infoText'>
+                Create code for hello world in Python, Javascript and C++.
+              </p>
+            </div>
           </div>
-        </div>
-        <div>
-          <div className='flex flex-col items-center justify-center mb-5'>
-            <BoltIcon className='h-8 w-8' />
-            <h2>Capabilities</h2>
-          </div>
+          <div>
+            <div className='flex flex-col items-center justify-center mb-5'>
+              <BoltIcon className='h-8 w-8' />
+              <h2>Capabilities</h2>
+            </div>
 
-          <div className='space-y-2'>
-            <p className='infoText'>Find and post images</p>
-            <p className='infoText'>Chats are stored in a Firestore database</p>
-            <p className='infoText'>
-              Advanced text formatting for responses, including code formatting
-              and styling.
-            </p>
+            <div className='space-y-2'>
+              <p className='infoText'>Find and post images</p>
+              <p className='infoText'>
+                Chats are stored in a Firestore database
+              </p>
+              <p className='infoText'>
+                Advanced text formatting for responses, including code
+                formatting and styling.
+              </p>
+            </div>
           </div>
-        </div>
-        <div>
-          <div className='flex flex-col items-center justify-center mb-5'>
-            <ExclamationTriangleIcon className='h-8 w-8' />
-            <h2>Limitation</h2>
-          </div>
+          <div>
+            <div className='flex flex-col items-center justify-center mb-5'>
+              <ExclamationTriangleIcon className='h-8 w-8' />
+              <h2>Limitation</h2>
+            </div>
 
-          <div className='space-y-2'>
-            <p className='infoText'>
-              May sometimes make up answers or give false information
-            </p>
-            <p className='infoText'>
-              Will frequently post images of the wrong things.
-            </p>
-            <p className='infoText'>
-              Has very limited knowledge of events after 2021
-            </p>
+            <div className='space-y-2'>
+              <p className='infoText'>
+                May sometimes make up answers or give false information
+              </p>
+              <p className='infoText'>
+                Will frequently post images of the wrong things.
+              </p>
+              <p className='infoText'>
+                Has very limited knowledge of events after 2021
+              </p>
+            </div>
           </div>
         </div>
       </div>
+      {/* create NewChatInput that creates a new chat i<NewChatInput chatId={id} divHeight={divHeight} /> */}
     </div>
   );
 }
