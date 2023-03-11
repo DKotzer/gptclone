f6640c90d42597deb04afd33c102cd6b322ba163
Dylan <34380246+DKotzer@users.noreply.github.com>
2023-03-11 09:41:43 -0500

    smaller font on home page

--- app/page.tsx
+++ app/page.tsx
@@ -125,19 +125,17 @@ function HomePage() {
   }
 
   return (
-    <div className={`flex flex-col h-screen overflow-hidden`}>
-      <div
-        className={`flex flex-col items-center justify-center h-full text-white px-2`}
-      >
+    <div className='flex flex-col h-screen overflow-hidden'>
+      <div className='flex flex-col items-center justify-center h-full text-white px-2'>
         <h1 className='text-5xl font-bold mb-[5%]'>DylanGPT</h1>
-        <div className='flex space-x-2 text-center'>
+        <div className='flex space-x-2 text-center text-sm'>
           <div>
             <div className='flex flex-col items-center justify-center mb-5'>
               <SunIcon className='h-8 w-8' />
               <h2>Examples</h2>
             </div>
 
-            <div className='space-y-2'>
+            <div className='space-y-1'>
               <p
                 onClick={promptSetter}
                 className='infoText border-slate-300/50 hover:border hover:scale-105 hover:cursor-pointer '
@@ -164,7 +162,7 @@ function HomePage() {
               <h2>Capabilities</h2>
             </div>
 
-            <div className='space-y-2'>
+            <div className='space-y-1'>
               <p
                 onClick={promptSetter}
                 className='infoText border-slate-300/50 hover:border hover:scale-105 hover:cursor-pointer'
@@ -186,7 +184,7 @@ function HomePage() {
               <h2>Limitation</h2>
             </div>
 
-            <div className='space-y-2'>
+            <div className='space-y-1'>
               <p className='infoText'>
                 May sometimes make up answers or give false information
               </p>
