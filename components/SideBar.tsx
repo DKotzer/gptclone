fb171ec0d35f7e9bd4d27f04db08c796565344bc
Dylan <34380246+DKotzer@users.noreply.github.com>
2023-03-13 12:17:59 -0400

    mobile only troubleshooting

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -66,7 +66,7 @@ function SideBar() {
           </svg>
         </button>
         <NewChat />
-        <div className='flex-1 max-w-xs pt-2  overflow-y-scroll'>
+        <div className='flex-1 max-w-xs pt-2  overflow-y-scroll max-h-[50%]'>
           <div className='pt-1 '>
             <div className='flex flex-col space-y-2 my-2 '>
               {loading && (
