d0955947260fbd4ef077ca97d5b936a2661447d6
Dylan <34380246+DKotzer@users.noreply.github.com>
2023-03-13 13:21:04 -0400

    trying something

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -38,7 +38,7 @@ function SideBar() {
   };
 
   return (
-    <div className='max-h-screen overflow-y-scroll'>
+    <div className='max-h-screen '>
       <div
         className={`${
           hidden && "hidden"
@@ -153,7 +153,7 @@ function SideBar() {
             >
               <ArrowUpTrayIcon className='h-4 w-4 rotate-90' />
             </div>
-            {/* <div className='h-[70px]  md:h-[35px]'></div> */}
+            <div className='max-h-[70px] min-h-[10px]  md:h-[35px]'></div>
           </div>
         )}
       </div>
