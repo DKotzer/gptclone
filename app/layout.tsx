4e1ae30260e162036cdcdfd0737813e37fa6d7a8
Dylan <34380246+DKotzer@users.noreply.github.com>
2023-03-30 18:11:28 -0400

    moved to head.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,28 +21,7 @@ export default async function RootLayout({
 
   return (
     <html lang='en'>
-      <head>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        {/* <meta name='image' property='og:image' content='/layoutLarge.png'></meta> */}
-        <meta
-          name='image'
-          property='og:image'
-          content='https://gpt.dylankotzer.com/DylanGPTLogo.webp'
-        />
-        <meta
-          name='image'
-          property='og:image'
-          content='https://gpt.dylankotzer.com/LogoLarge.png'
-        />
-
-        <meta property='og:title' content='DylanGPT' />
-        <meta
-          property='og:description'
-          content='An above average GPT-powered Chatbot created by Dylan Kotzer. Give it a shot!'
-        />
-
-        <meta name='author' content='Dylan Kotzer'></meta>
-      </head>
+      <head></head>
       <body>
         <SessionProvider session={session}>
           {!session ? (
