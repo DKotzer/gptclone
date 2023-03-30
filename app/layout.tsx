b8303366606967eae95e5e4fd3e1b0bae3007434
Dylan <34380246+DKotzer@users.noreply.github.com>
2023-03-30 16:08:13 -0400

    og images are fun

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default async function RootLayout({
   return (
     <html lang='en'>
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      <meta name='image' property='og:image' content='/layoutLarge.png'></meta>
+      {/* <meta name='image' property='og:image' content='/layoutLarge.png'></meta> */}
       <meta
         name='image'
         property='og:image'
