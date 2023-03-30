a15dee0f6081692413ab23b1718eedc1dc6cd089
Dylan <34380246+DKotzer@users.noreply.github.com>
2023-03-30 16:11:49 -0400

    wat

--- app/head.tsx
+++ app/head.tsx
@@ -7,20 +7,6 @@ export default function Head() {
       <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <link rel='icon' href='/dgpt.webp' />
-      <meta name='image' property='og:image' content='/layoutLarge.png'></meta>
-      <meta
-        name='image'
-        property='og:image'
-        content='https://gpt.dylankotzer.com/DylanGPTLogo.webp'
-      />
-      <meta property='og:title' content='DylanGPT' />
-      <meta
-        property='og:description'
-        content='An above average GPT-powered Chatbot created by Dylan Kotzer. Give it a shot!'
-      />
-      <head />
-      <meta name='author' content='Dylan Kotzer'></meta>
-
       <link
         rel='stylesheet'
         href='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/atom-one-dark-reasonable.min.css'
@@ -28,3 +14,19 @@ export default function Head() {
     </>
   );
 }
+
+
+
+      // <meta name='image' property='og:image' content='/layoutLarge.png'></meta>
+      // <meta
+      //   name='image'
+      //   property='og:image'
+      //   content='https://gpt.dylankotzer.com/DylanGPTLogo.webp'
+      // />
+      // <meta property='og:title' content='DylanGPT' />
+      // <meta
+      //   property='og:description'
+      //   content='An above average GPT-powered Chatbot created by Dylan Kotzer. Give it a shot!'
+      // />
+      // <head />
+      // <meta name='author' content='Dylan Kotzer'></meta>
