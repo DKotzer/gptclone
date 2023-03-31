24ebc1a387df768c008fe00df1cc114d6f2112b9
Dylan <34380246+DKotzer@users.noreply.github.com>
2023-03-30 21:38:59 -0400

    um

--- app/head.tsx
+++ app/head.tsx
@@ -11,32 +11,17 @@ export default function Head() {
         rel='stylesheet'
         href='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/atom-one-dark-reasonable.min.css'
       />
-
       <meta
         name='image'
         property='og:image'
         content='https://gpt.dylankotzer.com/DylanGPTLogo.webp'
       />
-
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      {/* <meta name='image' property='og:image' content='/layoutLarge.png'></meta> */}
-      {/* <meta
-        name='image'
-        property='og:image'
-        content='https://gpt.dylankotzer.com/DylanGPTLogo.webp'
-      />
-      <meta
-        name='image'
-        property='og:image'
-        content='https://gpt.dylankotzer.com/LogoLarge.png'
-      /> */}
-
       <meta property='og:title' content='DylanGPT' />
       <meta
         property='og:description'
         content='An above average GPT-powered Chatbot created by Dylan Kotzer. Give it a shot!'
       />
-
       <meta name='author' content='Dylan Kotzer'></meta>
     </>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -30,31 +30,12 @@ export default async function RootLayout({
           rel='stylesheet'
           href='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/atom-one-dark-reasonable.min.css'
         />
-
-        {/* <meta
-          name='image'
-          property='og:image'
-          content='https://gpt.dylankotzer.com/DylanGPTLogo.webp'
-        /> */}
         <meta
           name='image'
           property='og:image'
           content='https://gpt.dylankotzer.com/LogoLarge.jpeg'
         />
-
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        {/* <meta name='image' property='og:image' content='/layoutLarge.png'></meta> */}
-        {/* <meta
-        name='image'
-        property='og:image'
-        content='https://gpt.dylankotzer.com/DylanGPTLogo.webp'
-      />
-      <meta
-        name='image'
-        property='og:image'
-        content='https://gpt.dylankotzer.com/LogoLarge.png'
-      /> */}
-
         <meta property='og:title' content='DylanGPT' />
         <meta
           property='og:description'
